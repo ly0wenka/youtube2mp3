d91ed4a7cf2c415117e14ae6565f30058477631e
ly0wenka <ly0wenka@gmail.com>
2023-02-22 14:56:36 +0200

    'commit'

--- Program.cs
+++ Program.cs
@@ -11,9 +11,9 @@ class Program
         var videoId = "https://youtu.be/DD6o3f2-lVI"; // Replace with the actual ID of the YouTube video you want to download
         var client = new YoutubeClient();
         var video = await client.Videos.GetAsync(videoId);
-        var streamInfoSet = await client.Videos.GetMediaStreamInfosAsync(videoId);
+        var streamManifest = await client.Videos.Streams.GetManifestAsync(videoId);
         var audioStreamInfo = streamInfoSet.Audio.WithHighestBitrate();
-        var audioStream = await client.Videos.GetVideoMediaStreamInfosAsync(audioStreamInfo);
+        var stream = await client.Videos.Streams.GetAsync(audioStreamInfo);
 
         var outputFilePath = video.Title + ".mp3";
         using (var outputFile = File.Create(outputFilePath))
