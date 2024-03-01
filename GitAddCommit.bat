6cd9e137ff88af02056176cfad0f8dfa8c67b978
ly0wenka <ly0wenka@gmail.com>
2024-03-01 20:35:01 +0200

    'Fix'

--- GitAddCommit.bat
+++ GitAddCommit.bat
@@ -1,1 +1,1 @@
-git add . & git commit -m 'commit'
+git add . & git commit -m 'Fix'
--- Program.cs
+++ Program.cs
@@ -8,17 +8,24 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        var videoId = "https://youtu.be/DD6o3f2-lVI"; // Replace with the actual ID of the YouTube video you want to download
+        var videoId = "https://www.youtube.com/watch?v=je5zd1nFdyk&ab_channel=Ado-Topic"; // Replace with the actual ID of the YouTube video you want to download
         var client = new YoutubeClient();
         var video = await client.Videos.GetAsync(videoId);
         var streamManifest = await client.Videos.Streams.GetManifestAsync(videoId);
-        var audioStreamInfo = streamInfoSet.Audio.WithHighestBitrate();
+        
+        // Get the audio streams from the manifest
+        var audioStreams = streamManifest.GetAudioOnlyStreams();
+        
+        // Find the audio stream with the highest bitrate
+        var audioStreamInfo = audioStreams.OrderByDescending(s => s.Bitrate).First();
+        
+        // Get the actual stream
         var stream = await client.Videos.Streams.GetAsync(audioStreamInfo);
 
         var outputFilePath = video.Title + ".mp3";
         using (var outputFile = File.Create(outputFilePath))
         {
-            await audioStream.CopyToAsync(outputFile);
+            await stream.CopyToAsync(outputFile);
         }
     }
 }
